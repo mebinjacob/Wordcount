d60773ce9f5ec9113b15ff43ed74a670f6028949
Mebin Jacob <mebinjacob@gmail.com>
2015-10-07 04:50:23 -0400

    initial commit

---
+++ src/main/java/WordCount.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class WordCount extends Configured implements Tool {
+
+    public static void main(String[] args) throws Exception {
+        int res = ToolRunner.run(new Configuration(), new WordCount(), args);
+        System.exit(res);
+	}
+
+    public int run(String args[]) {
+        try {
+            Configuration conf = new Configuration();
+
+            Job job = Job.getInstance(conf);
+            job.setJarByClass(WordCount.class);
+
+            // specify a mapper
+            job.setMapperClass(WordCountMapper.class);
+
+            // specify a reducer
+            job.setReducerClass(WordCountReducer.class);
+
+            // specify output types
+            job.setOutputKeyClass(Text.class);
+            job.setOutputValueClass(IntWritable.class);
+
+            // specify input and output DIRECTORIES
+            FileInputFormat.addInputPath(job, new Path(args[0]));
+            job.setInputFormatClass(TextInputFormat.class);
+
+            FileOutputFormat.setOutputPath(job, new Path(args[1]));
+            job.setOutputFormatClass(TextOutputFormat.class);
+
+            return(job.waitForCompletion(true) ? 0 : 1);
+        } catch (InterruptedException|ClassNotFoundException|IOException e) {
+            System.err.println("Error during mapreduce job.");
+            e.printStackTrace();
+            return 2;
+        }
+    }
+}
---
+++ src/main/java/WordCountMapper.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
+	
+    private final IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        StringTokenizer itr = new StringTokenizer(line.toLowerCase(), " .,:;!?{}[]()|_");
+        while(itr.hasMoreTokens()) {
+            word.set(itr.nextToken());
+            context.write(word, one);
+        }
+    }
+} 
